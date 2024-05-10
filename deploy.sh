83837ab15e09a5542a970d8b6b8b53649d050788
Doğukaan Kılıçarslan <dogukaan.k@hotmail.com>
2024-05-10 20:46:07 +0300

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-# Add GitHub to the known hosts
-mkdir -p ~/.ssh
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-
-# Clone your repository
+# Go to the user's home directory
 cd /home/$AWS_USERNAME
+
+# If the repository directory doesn't exist, clone it
 if [ ! -d "$APP_NAME" ]; then
   git clone $REPOSITORY_URL
 fi
 
-# Pull the latest changes
+# Navigate to the repository directory (which is now guaranteed to exist)
 cd $APP_NAME
+
+# Pull the latest changes
 git pull
 
-# Build your Go application
-go build -o $APP_EXECUTABLE
+# Build the Go application
+go build -o $APP_EXECUTABLE .
 
-# Create a new systemd service
+# Check if systemd service exists, if not, create one
 if [ ! -e "/etc/systemd/system/$APP_NAME.service" ]; then
-  sudo bash -c 'cat > /etc/systemd/system/$APP_NAME.service <<EOF
-  [Unit]
-  Description=$APP_NAME Service
-  After=network.target
+  sudo bash -c 'cat > /etc/systemd/system/$APP_NAME.service' <<EOF
+[Unit]
+Description=$APP_NAME Service
+After=network.target
 
-  [Service]
-  ExecStart=/home/$AWS_USERNAME/$APP_NAME/$APP_EXECUTABLE
-  WorkingDirectory=/home/$AWS_USERNAME/$APP_NAME
-  User=$AWS_USERNAME
-  Group=$AWS_USERNAME
-  Restart=always
+[Service]
+ExecStart=/home/$AWS_USERNAME/$APP_NAME/$APP_EXECUTABLE
+WorkingDirectory=/home/$AWS_USERNAME/$APP_NAME
+User=$AWS_USERNAME
+Group=$AWS_USERNAME
+Restart=always
 
-  [Install]
-  WantedBy=multi-user.target
-  EOF'
+[Install]
+WantedBy=multi-user.target
+EOF
 fi
 
 # Start the application service
@@ -42,5 +42,5 @@ sudo systemctl start $APP_NAME
 # Enable the application service
 sudo systemctl enable $APP_NAME
 
-# Check the status of your service
+# Check the status of the service
 sudo systemctl status $APP_NAME
